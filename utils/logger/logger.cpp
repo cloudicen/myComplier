13b318c658fce5db77f2e4413bf2322c4784edee
五仁面包人 <36070534+cloudicen@users.noreply.github.com>
2021-08-31 22:05:56 +0800

    替换字符串拼接为log4cpp的内置格式化

--- utils/logger/logger.cpp
+++ utils/logger/logger.cpp
@@ -6,20 +6,20 @@
 
 void logger::debug(std::string msg) {
     //this->log4cppCategory->debug(std::format("{[]} {}",moduleName,msg));
-    this->log4cppCategory.debug(std::string("["+moduleName+"] "+msg));
+    this->log4cppCategory.debug("[%s] %s",moduleName.c_str(),msg.c_str());
 }
 
 void logger::info(std::string msg) {
     //this->log4cppCategory->info(std::format("{[]} {}",moduleName,msg));
-    this->log4cppCategory.info(std::string("["+moduleName+"] "+msg));
+    this->log4cppCategory.info("[%s] %s",moduleName.c_str(),msg.c_str());
 }
 
 void logger::warn(std::string msg) {
     //this->log4cppCategory->warn(std::format("{[]} {}",moduleName,msg));
-    this->log4cppCategory.warn(std::string("["+moduleName+"] "+msg));
+    this->log4cppCategory.warn("[%s] %s",moduleName.c_str(),msg.c_str());
 }
 
 void logger::error(std::string msg) {
     //this->log4cppCategory->error(std::format("{[]} {}",moduleName,msg));
-    this->log4cppCategory.error(std::string("["+moduleName+"] "+msg));
+    this->log4cppCategory.error("[%s] %s",moduleName.c_str(),msg.c_str());
 }
