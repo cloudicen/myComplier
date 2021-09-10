13f620996402daed19c9df2c489f7bb5aec68e9a
五仁面包人 <36070534+cloudicen@users.noreply.github.com>
2021-09-11 01:10:27 +0800

    修复了括号匹配时的逻辑错误，并重新格式化了代码

--- regEngine/RegTree.cpp
+++ regEngine/RegTree.cpp
@@ -4,143 +4,141 @@
 
 #include "RegTree.h"
 
-std::unique_ptr<regEngine::RegNode> &regEngine::RegTree::generateRegTree() {
-  root.reset(parseRegExpr());
-  return root;
+const regEngine::RegNode *regEngine::RegTree::generateRegTree() {
+    root.reset(parseRegExpr());
+    return root.get();
 }
 
 void regEngine::RegTree::goNext() {
-  if (currentMatchPos + 1 < regExpression.length()) {
-    currentMatchPos++;
-    currentMatchChar = regExpression.at(currentMatchPos);
-  } else {
-    currentMatchPos = regExpression.length();
-    currentMatchChar = 0;
-  }
+    if (currentMatchPos + 1 < regExpression.length()) {
+        currentMatchPos++;
+        currentMatchChar = regExpression.at(currentMatchPos);
+    } else {
+        currentMatchPos = regExpression.length();
+        currentMatchChar = 0;
+    }
 }
 
 bool regEngine::RegTree::match(char ch) { return currentMatchChar == ch; }
 
 void regEngine::RegTree::matchPair_push(char ch) {
-  char target = 0;
-  target = signInPair.at(ch);
-  matchPairStack.push(target);
+    char target = 0;
+    target = signInPair.at(ch);
+    matchPairStack.push(target);
 }
 
 bool regEngine::RegTree::matchPair_match(char ch) {
-  if (matchPairStack.empty()) {
-    return false;
-  }
-  if (matchPairStack.top() == ch) {
-    matchPairStack.pop();
-    return true;
-  } else {
-    return false;
-  }
+    if (matchPairStack.empty()) {
+        return false;
+    }
+    if (matchPairStack.top() == ch) {
+        matchPairStack.pop();
+        return true;
+    } else {
+        return false;
+    }
 }
 
 regEngine::RegNode *regEngine::RegTree::parseRegExpr() {
-  LOGGER->debug("parseRegExpr: begin");
-  if (match('.') || (isprint(currentMatchChar) && !keptCharSet.contains(currentMatchChar))) {
-    LOGGER->debug("match: '" + std::string(&currentMatchChar) + "'");
-    auto cur = new regEngine::RegNode(Element, std::string({currentMatchChar}));
-    goNext();
-    auto f = parseSubExpr(cur);
-    if (f != nullptr) {
-      return f;
+    LOGGER->debug("parseRegExpr: begin");
+    if (match('.') || (isprint(currentMatchChar) && !keptCharSet.contains(currentMatchChar))) {
+        LOGGER->debug("match: '" + std::string(&currentMatchChar) + "'");
+        auto cur = new regEngine::RegNode(Element, std::string({currentMatchChar}));
+        goNext();
+        auto f = parseSubExpr(cur);
+        if (f != nullptr) {
+            return f;
+        } else {
+            return cur;
+        }
+    } else if (match('\\')) {
+        LOGGER->debug("match: '\\'");
+        goNext();
+        auto cur = parseEscapeChar();
+        auto f = parseSubExpr(cur);
+        if (f != nullptr) {
+            return f;
+        } else {
+            return cur;
+        }
+    } else if (match('(')) {
+        LOGGER->debug("match: '('");
+        matchPair_push(currentMatchChar);
+        goNext();
+        auto cur = parseRegExpr();
+        if (matchPair_match(currentMatchChar)) {
+            goNext();
+            LOGGER->debug("match: ')'");
+            auto f = parseSubExpr(cur);
+            if (f != nullptr) {
+                return f;
+            } else {
+                return cur;
+            }
+        } else {
+            LOGGER->error("reg syntax error, missing ')'");
+            return nullptr;
+        }
     } else {
-      return cur;
+        if (currentMatchPos == regExpression.length() - 1) {
+            LOGGER->debug("parseRegExpr: match end");
+        } else {
+            LOGGER->debug("parseRegExpr: match empty");
+        }
+        return nullptr;
     }
-  } else if (match('\\')) {
-    LOGGER->debug("match: '\\'");
-    goNext();
-    auto cur = parseEscapeChar();
-    auto f = parseSubExpr(cur);
-    if (f != nullptr) {
-      return f;
-    } else {
-      return cur;
-    }
-  } else if (match('(')) {
-    LOGGER->debug("match: '('");
-    matchPair_push(currentMatchChar);
-    goNext();
-    auto cur = parseRegExpr();
-    if (matchPair_match(currentMatchChar)) {
-      LOGGER->debug("match: ')'");
-      auto f = parseSubExpr(cur);
-      if (f != nullptr) {
-        return f;
-      } else {
-        return cur;
-      }
-    } else {
-      LOGGER->error("reg syntax error, missing ')'");
-      return nullptr;
-    }
-  } else {
-    if (currentMatchPos == regExpression.length() - 1) {
-      LOGGER->debug("parseRegExpr: match end");
-    } else {
-      LOGGER->error("parseRegExpr: match error");
-    }
-    return nullptr;
-  }
 }
 
 regEngine::RegNode *regEngine::RegTree::parseSubExpr(RegNode *regExprNode) {
-  LOGGER->debug("parseSubExpr: begin");
-  if (match('|')) {
-    LOGGER->debug("match '|'");
-    goNext();
-    auto r = parseRegExpr();
-    auto cur =
-        new regEngine::RegNode(Alternate, "alternate",
-                               std::unique_ptr<regEngine::RegNode>(regExprNode),
-                               std::unique_ptr<regEngine::RegNode>(r));
-    auto f = parseSubExpr(cur);
-    if (f != nullptr) {
-      return f;
+    LOGGER->debug("parseSubExpr: begin");
+    if (match('|')) {
+        LOGGER->debug("match '|'");
+        goNext();
+        auto r = parseRegExpr();
+        auto cur = new regEngine::RegNode(Alternate, "alternate", std::unique_ptr<regEngine::RegNode>(regExprNode),
+                                          std::unique_ptr<regEngine::RegNode>(r));
+        auto f = parseSubExpr(cur);
+        if (f != nullptr) {
+            return f;
+        } else {
+            return cur;
+        }
+    } else if (match('*')) {
+        LOGGER->debug("match '*'");
+        goNext();
+        auto cur = new regEngine::RegNode(Closure, "closure", std::unique_ptr<regEngine::RegNode>(regExprNode));
+        auto f = parseSubExpr(cur);
+        if (f != nullptr) {
+            return f;
+        } else {
+            return cur;
+        }
     } else {
-      return cur;
+        auto cur = parseRegExpr();
+        if (cur != nullptr) {
+            return new regEngine::RegNode(
+                    Concat, "concat", std::unique_ptr<regEngine::RegNode>(regExprNode),
+                    std::unique_ptr<regEngine::RegNode>(cur));
+        } else {
+            if (currentMatchPos == regExpression.length() - 1) {
+                LOGGER->debug("parseSubExpr: match end");
+            } else {
+                LOGGER->debug("parseSubExpr: match empty");
+            }
+            return nullptr;
+        }
     }
-  } else if (match('*')) {
-    LOGGER->debug("match '*'");
-    goNext();
-    auto cur = new regEngine::RegNode(
-        Closure, "closure", std::unique_ptr<regEngine::RegNode>(regExprNode));
-    auto f = parseSubExpr(cur);
-    if (f != nullptr) {
-      return f;
-    } else {
-      return cur;
-    }
-  } else {
-    auto cur = parseRegExpr();
-    if (cur != nullptr) {
-      return new regEngine::RegNode(
-          Concat, "concat", std::unique_ptr<regEngine::RegNode>(regExprNode),
-          std::unique_ptr<regEngine::RegNode>(cur));
-    } else {
-      if (currentMatchPos == regExpression.length() - 1) {
-        LOGGER->debug("parseSubExpr: match end");
-      } else {
-        LOGGER->error("parseSubExpr: match error");
-      }
-      return nullptr;
-    }
-  }
 }
 
 regEngine::RegNode *regEngine::RegTree::parseEscapeChar() {
-  LOGGER->debug("parseEscapeChar: begin\n");
-  if (escapeCharDef.contains(currentMatchChar)) {
-    LOGGER->debug("match '" + std::string(&currentMatchChar) + "'");
-    auto info = escapeCharDef.find(currentMatchChar)->second;
-    goNext();
-    return new regEngine::RegNode(Element, info);
-  } else {
-    LOGGER->error("parseEscapeChar: match error\n");
-    return nullptr;
-  }
+    LOGGER->debug("parseEscapeChar: begin\n");
+    if (escapeCharDef.contains(currentMatchChar)) {
+        LOGGER->debug("match '" + std::string(&currentMatchChar) + "'");
+        auto info = escapeCharDef.find(currentMatchChar)->second;
+        goNext();
+        return new regEngine::RegNode(Element, info);
+    } else {
+        LOGGER->error("parseEscapeChar: match error\n");
+        return nullptr;
+    }
 }
--- regEngine/RegTree.h
+++ regEngine/RegTree.h
@@ -18,50 +18,52 @@
 
 namespace regEngine {
 
-class RegTree {
-private:
-  std::shared_ptr<logger> LOGGER = LoggerFactory::getLogger("regTree");
-  /* -------------------------需要转换的表达式----------------------------- */
-  std::string regExpression = "";
-  /* ------------------------------------------------------------------- */
+    class RegTree {
+    private:
+        std::shared_ptr<logger> LOGGER = LoggerFactory::getLogger("regTree");
+        /* -------------------------需要转换的表达式----------------------------- */
+        std::string regExpression = "";
+        /* ------------------------------------------------------------------- */
 
-  std::unique_ptr<RegNode> root = nullptr;
+        std::unique_ptr<RegNode> root = nullptr;
 
-public:
-  explicit RegTree(const std::string &_regExpression)
-      : regExpression(_regExpression), currentMatchChar(_regExpression.at(0)){};
+    public:
+        explicit RegTree(const std::string &_regExpression)
+                : regExpression(_regExpression), currentMatchChar(_regExpression.at(0)) {};
 
-  std::unique_ptr<RegNode> &generateRegTree();
+        RegTree(const RegTree &other) = delete;
 
-private:
-  int currentMatchPos = 0;
-  char currentMatchChar = 0;
-  std::stack<char> matchPairStack;
+        const RegNode* generateRegTree();
 
-private:
-  void goNext();
+    private:
+        int currentMatchPos = 0;
+        char currentMatchChar = 0;
+        std::stack<char> matchPairStack;
 
-  bool match(char ch);
+    private:
+        void goNext();
 
-  void matchPair_push(char ch);
+        bool match(char ch);
 
-  bool matchPair_match(char ch);
+        void matchPair_push(char ch);
 
-  /*
-   * 这里直接将语义操作（生成语法树结构）嵌入递归下降子程序。
-   * 用了三个函数，对应三个非终结符，但函数的形式并不相同。
-   * 这与先前对文法的定义有关。其中regExpr、escapeChar的开头都是基本元素节点，都是作为叶子节点，子节点为空（这里将(regExpr)整体是为一个节点，道理是一样的）。
-   * 而subExpr的开头是‘|’或‘*’,其拥有子节点。
-   * 为了在递归过程中正确构建节点间的树形关系，parseSubExpr(RegNode *
-   * regExprNode)添加了额外的参数，用于传入其子节点;
-   */
+        bool matchPair_match(char ch);
 
-  RegNode *parseRegExpr();
+        /*
+         * 这里直接将语义操作（生成语法树结构）嵌入递归下降子程序。
+         * 用了三个函数，对应三个非终结符，但函数的形式并不相同。
+         * 这与先前对文法的定义有关。其中regExpr、escapeChar的开头都是基本元素节点，都是作为叶子节点，子节点为空（这里将(regExpr)整体是为一个节点，道理是一样的）。
+         * 而subExpr的开头是‘|’或‘*’,其拥有子节点。
+         * 为了在递归过程中正确构建节点间的树形关系，parseSubExpr(RegNode *
+         * regExprNode)添加了额外的参数，用于传入其子节点;
+         */
 
-  RegNode *parseSubExpr(RegNode *regExprNode);
+        RegNode *parseRegExpr();
 
-  RegNode *parseEscapeChar();
-};
+        RegNode *parseSubExpr(RegNode *regExprNode);
+
+        RegNode *parseEscapeChar();
+    };
 } // namespace regEngine
 
 #endif // MY_COMPILER_REGTREE_H
